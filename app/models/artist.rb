ac35c934d684f9323310ace292557a1295b1858d
sin wan wildy <sw.wildy@gmail.com>
2021-01-19 10:47:52 -0800

    commit tree b776133df39c497f0fb6075547ec4782c72aeb93

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,23 @@
 class Artist
 
-  attr_reader :name, :years_experience
+  
 
+  attr_reader :name, :years_experience
+  @@all = []
+   
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def paintings 
+    
+  end
+
+  
 end
